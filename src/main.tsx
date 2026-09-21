c7b0234d093871969f458655a8700b298200512c
davisolpruvnyk <davisolpruvnyk@atomicmail.io>
2026-09-21 19:39:32 +0800

    chore(styles): remove unused font options in index.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,6 @@ import App from './App.tsx';
 import './fonts.css';
 import './index.css';
 import './i18n';
-import '@fontsource/rajdhani/300.css';
-import '@fontsource/rajdhani/400.css';
-import '@fontsource/rajdhani/500.css';
-import '@fontsource/rajdhani/600.css';
-import '@fontsource/rajdhani/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
